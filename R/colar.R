d9d69e13e8c934fb691602c05d3c7670b7739b0e
jtrecenti <julio.trecenti@gmail.com>
2020-04-28 00:31:44 +0000

    Initial commit

---
+++ R/colar.R
@@ -0,0 +1,14 @@
+
+#' Cola x e y
+#'
+#' Recebe x e y e retorna eles colados
+#'
+#' @param x primeiro parametro
+#' @param y segundo parametro
+#'
+#' @return x e y colados
+#'
+#' @export
+colar <- function(x, y) {
+  paste(x, y)
+}
